8887a9f112688e0ce96604a9c13f48d17d541fbd
Mattia Farabegoli <94555625+mattiafarabegoli@users.noreply.github.com>
2023-06-24 12:24:52 +0200

    Delete updates-plugin/updates-plugin directory

--- updates-plugin/updates-plugin/class-quiet-skin.php
+++
@@ -1,6 +0,0 @@
-<?php
-class Quiet_Skin extends WP_Upgrader_Skin {
-	public function feedback($string) {}
-	public function header() {}
-	public function footer() {}
-}
--- updates-plugin/updates-plugin/interface-up-updater.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-interface UP_Updater {
-	
-	public function check();
-	
-	public function update();
-	
-}
